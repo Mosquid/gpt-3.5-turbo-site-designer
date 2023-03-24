bf5b04627dfc55bbcb89a14359519b03fd4ce0dc
Dmitry Mosquid <mosquid@gmail.com>
2023-03-24 11:52:37 +0300

    initial

---
+++ pages/api/designer.ts
@@ -0,0 +1,64 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+
+const API_KEY = process.env.API_KEY;
+
+const configuration = new Configuration({
+  apiKey: API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { story, content } = req.body;
+  const messages: Array<ChatCompletionRequestMessage> = [
+    {
+      content: `Hey. I have a simple html website. Here is its source code: ${content}. I need to add some funky css to it. Here are some ideas: ${story}. Could you please update to source code to make it look like this? Thanks!`,
+      role: "user",
+    },
+
+    {
+      role: "assistant",
+      content: "here you go",
+    },
+    {
+      role: "user",
+      content:
+        "thanks. could you wrap it in a code block and ship it with no formatting or line breaks?",
+    },
+  ];
+
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      // max_tokens: 10,
+      messages,
+    });
+    const { message } = response.data.choices[0];
+    console.log(message);
+    const regex = /<html lang=\"en\">[\s\S]*<\/html>/;
+
+    let { content } = message || {};
+    const match = (content || "").match(regex);
+
+    if (match) {
+      content = match[0];
+    }
+
+    res.status(200).json({ content });
+  } catch (error) {
+    // @ts-ignore
+    console.log(error);
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "1mb",
+    },
+  },
+};
