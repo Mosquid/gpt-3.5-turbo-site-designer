2355a9acb3003580ec2b0f5fa48700bbc3469a73
Dmitry Mosquid <mosquid@gmail.com>
2023-03-26 20:14:00 +0300

    redis secrets added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     API_KEY: process.env.API_KEY,
+    REDIS_ENDPOINT: process.env.REDIS_ENDPOINT,
+    REDIS_PASSWORD: process.env.REDIS_PASSWORD,
   },
 };
 
